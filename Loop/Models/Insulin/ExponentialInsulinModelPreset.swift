6006d1a57488e62d99166a99a5c6fa2312b03085
David Daniels <ddaniels1@gmail.com>
2018-06-22 14:47:40 -0700

    set DIA to 240 minutes for FIASP

--- Loop/Models/Insulin/ExponentialInsulinModelPreset.swift
+++ Loop/Models/Insulin/ExponentialInsulinModelPreset.swift
@@ -24,7 +24,7 @@ extension ExponentialInsulinModelPreset {
         case .humalogNovologChild:
             return .minutes(360)
         case .fiasp:
-            return .minutes(210)
+            return .minutes(240)
         }
     }
 
@@ -35,7 +35,7 @@ extension ExponentialInsulinModelPreset {
         case .humalogNovologChild:
             return .minutes(65)
         case .fiasp:
-            return .minutes(60)
+            return .minutes(55)
         }
     }
 
