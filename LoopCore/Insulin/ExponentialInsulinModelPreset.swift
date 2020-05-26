32f984c46725570bc44662b3f8b5b5ba3a8d2807
DD <ddaniels1@gmail.com>
2020-05-25 19:30:29 -0700

    Drop DIA to 190

--- LoopCore/Insulin/ExponentialInsulinModelPreset.swift
+++ LoopCore/Insulin/ExponentialInsulinModelPreset.swift
@@ -24,7 +24,7 @@ extension ExponentialInsulinModelPreset {
         case .humalogNovologChild:
             return .minutes(360)
         case .fiasp:
-            return .minutes(240)
+            return .minutes(190)
         }
     }
 
@@ -46,7 +46,7 @@ extension ExponentialInsulinModelPreset {
         case .humalogNovologChild:
             return .minutes(10)
         case .fiasp:
-            return .minutes(10)
+            return .minutes(15)
         }
     }
 
